b65121b2aeb0038a22265eef825f859bd9630d87
Kristopher Wilson <kristopherwilson@gmail.com>
2013-08-27 22:18:31 -0400

    stubbing out event handling for project injection

--- module/Martha/Module.php
+++ module/Martha/Module.php
@@ -2,8 +2,10 @@
 
 namespace Martha;
 
+use Zend\Mvc\Application;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
+use Zend\View\ViewEvent;
 
 /**
  * Class Module
@@ -12,13 +14,34 @@ use Zend\Mvc\MvcEvent;
 class Module
 {
     /**
+     * @var \Zend\Mvc\Application
+     */
+    protected $application;
+
+    /**
      * @param MvcEvent $e
      */
     public function onBootstrap(MvcEvent $e)
     {
-        $eventManager = $e->getApplication()->getEventManager();
+        $this->application = $e->getApplication();
+
+        $eventManager = $this->application->getEventManager();
+
+        $sharedEvents = $eventManager->getSharedManager();
+        $sharedEvents->attach('Zend\View\View', ViewEvent::EVENT_RENDERER, [$this, 'onRender']);
+
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
+    }
+
+    /**
+     * Ensure that the view always has a list of projects.
+     *
+     * @param ViewEvent $event
+     */
+    public function onRender(ViewEvent $event)
+    {
+        $event->getModel()->projects = [];
     }
 
     /**
