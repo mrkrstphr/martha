bb33ae0956f07695cf71973baa2ebea0f28e48a5
Kristopher Wilson <kristopherwilson@gmail.com>
2013-09-13 11:19:44 -0400

    adding project provider register call

--- plugins/GitHub/Plugin.php
+++ plugins/GitHub/Plugin.php
@@ -12,6 +12,6 @@ class Plugin extends AbstractPlugin
 {
     public function init()
     {
-
+        $this->getPluginManager()->registerRemoteProjectProvider('Martha\Plugin\GitHub\RemoteProjectProvider');
     }
 }
