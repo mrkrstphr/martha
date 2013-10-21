7de66e29b1263c7b4272c72fb623921367938b77
Kristopher Wilson <kristopherwilson@gmail.com>
2013-10-20 20:25:39 -0400

    Implementing plugin views
    Rough implementation, needs some code cleanup, works for now

--- module/Martha/src/Martha/Controller/BuildController.php
+++ module/Martha/src/Martha/Controller/BuildController.php
@@ -6,9 +6,11 @@ use Martha\Core\Domain\Entity\Build;
 use Martha\Core\Domain\Repository\BuildRepositoryInterface;
 use Martha\Core\Domain\Repository\ProjectRepositoryInterface;
 use Martha\Core\Job\Queue;
+use Martha\Core\Plugin\ArtifactHandlers\DashboardWidgetProviderInterface;
+use Martha\Core\Plugin\ArtifactHandlers\TabProviderInterface;
+use Martha\View\Model\MarthaViewAdapter;
 use Zend\Mvc\Controller\AbstractActionController;
-use Martha\Core\Job\Trigger\GitHubWebHook\Factory as GithubWebHookFactory;
-use Zend\View\Model\JsonModel;
+use Zend\View\Model\ViewModel;
 
 /**
  * Class BuildController
@@ -39,27 +41,56 @@ class BuildController extends AbstractActionController
     }
 
     /**
-     * @return array
+     * @return ViewModel
      */
     public function viewAction()
     {
-        $id = $this->params('id');
-
+        $id = $this->params()->fromRoute('id');
         $build = $this->buildRepository->getById($id);
 
         $config = $this->getConfig();
 
         $outputDir = $config['data-directory'] . '/' . $build->getProject()->getName() . '/' . $id;
-
         $output = file_exists($outputDir . '/console.html') ?
             file_get_contents($outputDir . '/console.html') : 'None Available';
 
+        $viewModel = new ViewModel(
+            [
+                'pageTitle' => 'Build #' . $id,
+                'output' => $output,
+                'build' => $build
+            ]
+        );
 
-        return [
-            'pageTitle' => 'Build #' . $id,
-            'output' => $output,
-            'build' => $build
+        $artifacts = [
+            'dashboard' => [],
+            'tabbed' => []
         ];
+
+        foreach ($build->getArtifacts() as $artifact) {
+            $artifactHandler = $this->getServiceLocator()->get('System')
+                ->getPluginManager()->getArtifactHandler($artifact->getHelper());
+
+            if ($artifactHandler && $artifactHandler instanceof DashboardWidgetProviderInterface) {
+                $artifactHandler->parseArtifact($build, file_get_contents($artifact->getFile()));
+                $view = new MarthaViewAdapter($artifactHandler->getDashboardWidget());
+                $viewModel->addChild($view, $artifact->getHelper() . '-widget');
+
+                $artifacts['dashboard'][$artifact->getHelper() . '-widget'] = $view->getVariable('widgetTitle');
+            }
+
+            if ($artifactHandler && $artifactHandler instanceof TabProviderInterface) {
+                $artifactHandler->parseArtifact($build, file_get_contents($artifact->getFile()));
+                $view = new MarthaViewAdapter($artifactHandler->getTabbedPane());
+                $viewModel->addChild($view, $artifact->getHelper() . '-tab');
+
+                $artifacts['tabbed'][$artifact->getHelper() . '-tab'] = $view->getVariable('tabTitle');
+            }
+        }
+
+        $viewModel->setVariable('artifacts', $artifacts);
+
+        return $viewModel;
     }
 
     /**
