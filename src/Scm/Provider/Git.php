babec306aa1876d7b9fb81cf5f2823865b4dc865
Kristopher Wilson <kristopherwilson@gmail.com>
2014-11-23 16:02:09 -0500

    removing temporary debug output

--- src/Scm/Provider/Git.php
+++ src/Scm/Provider/Git.php
@@ -15,9 +15,7 @@ class Git extends AbstractProvider
     public function cloneRepository($cloneToPath)
     {
         $command = 'git clone ' . $this->repository . ' ' . $cloneToPath;
-        return $this->environment->runCommand($command, function ($type, $buffer) {
-            file_put_contents($this->logFile, $buffer, FILE_APPEND);
-        });
+        return $this->environment->runCommand($command);
     }
 
     /**
